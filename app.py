51bb95d7f5b704fd652cef5960be4575133f5f63
wmahanxw <ajdzbd86@gmail.com>
2026-08-20 05:03:28 +0330

    fix: correct SQLALCHEMY_TRACK_MODIFICATIONS syntax

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///bot.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS = False']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
